d1e7111560254bae61d7920dd43194626eed7846
Luke Liu <lukepcd@gmail.com>
2016-02-06 00:07:24 -0500

    Setup basic framework and add aboutpage

--- app/src/main/java/com/cl/pethomed/MainActivity.java
+++ app/src/main/java/com/cl/pethomed/MainActivity.java
@@ -13,16 +13,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
+ //      implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.aboutus_page);
+
+/*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+*/
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +35,9 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        */
 
+/*
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,8 +46,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+*/
     }
 
+
+    /*
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -51,6 +60,8 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
+    */
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,35 +78,53 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        /*
         if (id == R.id.action_settings) {
             return true;
         }
+        */
 
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+/*
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        //Handle menu item action
+        if (id == R.id.nav_item1) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_item2) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_item3) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_item4) {
+
+        } else if (id == R.id.nav_item5) {
+
+        } else if (id == R.id.nav_item6) {
+
+        } else if (id == R.id.nav_item7) {
+
+        } else if (id == R.id.nav_item8) {
+
+        } else if (id == R.id.nav_item9) {
+
+        } else if (id == R.id.nav_item10) {
+
+        } else if (id == R.id.nav_item11) {
 
         }
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+*/
+
 }
