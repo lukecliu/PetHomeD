773b7cd2fb0c2e9bd3297eabdfd175134942c107
Luke Liu <lukepcd@gmail.com>
2016-03-04 23:05:26 -0500

    add viewpager and it's adapter

--- app/src/main/java/com/cl/pethomed/HomepageFragment.java
+++ app/src/main/java/com/cl/pethomed/HomepageFragment.java
@@ -2,10 +2,14 @@ package com.cl.pethomed;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -16,6 +20,12 @@ public class HomepageFragment extends Fragment {
 
     ImageDescAdapter adapter;
     public ArrayList<ImageDescModel> model_list = new ArrayList<>();
+
+
+    ImageFragmentPagerAdapter imageFragmentPagerAdapter;
+    ViewPager viewPager;
+    static final int NUM_ITEMS = 6;
+    public static final String[] IMAGE_NAME = {"image0", "image1", "image2", "image3", "image4", "image5",};
 
     public HomepageFragment() {
     }
@@ -41,6 +51,9 @@ public class HomepageFragment extends Fragment {
 
         grid.setAdapter(adapter);
 
+        imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(this.getFragmentManager());
+        viewPager = (ViewPager) rootView.findViewById(R.id.hompage_viewpager);
+        viewPager.setAdapter(imageFragmentPagerAdapter);
         return rootView;
     }
 
@@ -59,5 +72,50 @@ public class HomepageFragment extends Fragment {
 
     }
 
+
+    public static class ImageFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public ImageFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            SwipeFragment fragment = new SwipeFragment();
+            return SwipeFragment.newInstance(position);
+        }
+
+
+    }
+
+
+    public static class SwipeFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootview = inflater.inflate(R.layout.viewpager_item_type1, container, false);
+
+            ImageView imageView = (ImageView) rootview.findViewById(R.id.item_imageview);
+            Bundle bundle = getArguments();
+            int position = bundle.getInt("position");
+            String imageFileName = IMAGE_NAME[position];
+            int imgResId = getResources().getIdentifier(imageFileName, "drawable", "com.cl.pethomed");
+            imageView.setImageResource(imgResId);
+            return rootview;
+        }
+
+        static SwipeFragment newInstance(int position) {
+            SwipeFragment swipeFragment = new SwipeFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("position", position);
+            swipeFragment.setArguments(bundle);
+            return swipeFragment;
+        }
+    }
 
 }
